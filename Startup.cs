a957a08954f43c73ff08de0db257952c5b229d2e
Nicolai Sättler <nicolaisattler1990@gmail.com>
2017-11-23 17:22:39 +0100

    develop: init repo

---
+++ Startup.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using CodeSquirl.Recipy.DataProvider;
+
+namespace CodeSquirl.Recipy.API
+{
+    public class Startup
+    {
+        private readonly string _databaseName;
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            _databaseName = "CodeSquirlRecipy";
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationContext>(options => options.UseInMemoryDatabase(_databaseName));
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
